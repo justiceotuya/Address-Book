479dc051ae9728d9fca388888a03a8a993c77bc9
Justice Otuya <jotuya2@gmail.com>
2018-03-26 13:14:08 +0100

    added

--- script/index.js
+++ script/index.js
@@ -4,7 +4,7 @@ var list = $("#contact-names");
 var parent = list.parent();
 
 //add name
-$("#submit-button").click(function() {
+$("#submit-button").click(function () {
   if ($("#contact").val() != "") {
     names.push($("#contact").val());
     console.log("contact Added");
@@ -16,12 +16,16 @@ $("#submit-button").click(function() {
   list
     .detach()
     .empty()
-    .each(function(i) {
+    .each(function (i) {
       for (var x = 0; x < names.length; x++) {
         $(this).append(
-          "<li><span class='user-circle'><i class='fas fa-user-circle'></i></span><span class='contact-name'>" +
-            names[x] +
-            "</span><div class='manipulate-name'><span class='edit-name'><i class='fas fa-edit'></i></span><span class='delete-name'><i class='fas fa-trash-alt' ></i></span></div></li>"
+          //   "<div class='nameList'><li><span class='user-circle'><i class='fas fa-user-circle'></i></span><span class='contact-name'>" +
+          //   names[x] +
+          //   "</span><div class='manipulate-name'><span class='edit-name'><i class='fas fa-edit'></i></span><span class='delete-name' onclick=deleteName()><i class='fas fa-trash-alt' ></i></span></div></li></div>"
+
+          "<div class='nameList'><li><span class='user-circle'><i class='fas fa-user-circle'></i></span><span class='contact-name'>" +
+          names[x] +
+          "</span><div class='manipulate-name'><button class='edit-name'>Edit</button></span><button class='delete-name'>Delete</button></span></div></li></div>"
         );
         if (x == names.length - 1) {
           $(this).appendTo(parent);
@@ -31,7 +35,9 @@ $("#submit-button").click(function() {
 });
 
 //delete name
-$("span.delete-name").on("click", function() {
-  console.log("Path selected");
-});
+$('button .delete-name').click(function () {
+  //for (var x = 0; x < names.length; x++) {
+  $(this).parentNode.remove();
+  // }
+})
 //edit name
